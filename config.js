b33b2248543d69d12f2292d48c8d6928aa21299e
Erick Zetta <erick.zetta@gmail.com>
2011-01-18 11:44:24 +0100

    dir module improvements

--- config.js
+++ config.js
@@ -3,12 +3,25 @@ exports.name = 'Config';
 
 exports.init = function(server) {
 	server.addConfig({
-		'documentRoot': '.'
+		'documentRoot': '.',
+		'indexDirectory': true
 	});
 };
 
 exports.request = function(c) {
 	if(c.location.match(/.php$/)) {
 		
+	}
+	
+	if(c.location.match(/^\/$/)) {
+		c.addConfig({
+			'indexDirectory': true
+		});
+	}
+	
+	console.log('VirtualHost', c.virtualHost);
+	
+	if(c.virtualHost == '127.0.0.1:8080') {
+		c.config.documentRoot = '/';
 	}
 };
