71ea43936fa8bf08e3d3ddd04e057899c0eb9e9c
Erick Zetta <erick.zetta@gmail.com>
2011-01-25 14:00:33 +0100

    Initial support for structured config files
    Json config example (see config.json)
    modules can add own properties

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-
-require('./lib/prototype');
-var HttpServer = require('./lib/httpServer').HttpServer;
-
+require.paths.unshift('./lib/');
 require.paths.unshift('./lib/external/');
+
+require('prototype');
+var HttpServer = require('httpServer').HttpServer;
 
 var s = new HttpServer();
 
+/* Basic module loading */
 s.loadModule('config');
-s.loadModule('file');
-s.loadModule('indexFiles');
-s.loadModule('autoindex');
-s.loadModule('access');
-s.loadModule('mime');
+s.loadModule('configJSON');
 
-s.loadConfig('./config.js');
+//s.loadModule('file');
+//s.loadModule('indexFiles');
+//s.loadModule('autoindex');
+//s.loadModule('access');
+//s.loadModule('mime');
 
+s.loadConfig('./config.json');
